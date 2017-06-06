4654b2be9b2e7bc8fc4a8cdf41f5d81a556979a7
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-06-06 20:38:45 +0530

    server

---
+++ models/event.js
@@ -0,0 +1,16 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var EventSchema   = new Schema({
+    name: String,
+    // event_id: String,
+    attending_count:Number,
+    interested_count:Number,
+    start_time: String,
+    end_time: String,
+    place: String,
+    timestamp: Number,
+    is_private: Boolean
+});
+
+module.exports = mongoose.model('Event', EventSchema);
--- server.js
+++ server.js
@@ -3,21 +3,100 @@
  */
 
 const express = require("express");
-const app= express();
+const app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var router = express.Router();
+var Event = require('./models/event.js');
 
-app.use(bodyParser.urlencoded({extended:true}));
+mongoose.connect('mongodb://rishz:eventsdb@ds111262.mlab.com:11262/events');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 var port = 8100;
 
+router.route('/events')
+
+    .post(function (req, res) {
+
+        var event = new Event;
+        event.name = req.body.name;
+        event.timestamp = req.body.timestamp;
+        // event.event_id = req.body.event_id;
+        event.attending_count = req.body.attending_count;
+        event.interested_count = req.body.interested_count;
+        event.start_time = req.body.start_time;
+        event.end_time = req.body.end_time;
+        event.place = req.body.place;
+        event.is_private = req.body.is_private;
+
+        event.save(function (err) {
+            if (err) {
+                res.send(err);
+            }
+            res.json({message: 'Event Created'});
+        })
+    })
+
+    .get(function (req, res) {
+        Event.find(function (err, events) {
+            if (err) {
+                res.send(err)
+            }
+            res.json(events)
+        })
+    });
+
 router.get('/', function (req, res) {
 
-    res.json({message:'welcome to events-api!'})
+    res.json({message: 'welcome to events-api!'})
 
 });
 
-app.use('/api',router);
+router.route('/event/:event_id')
+
+.get(function (req, res) {
+    Event.findById(req.params.event_id, function (err, event) {
+        if(err){
+            res.send(err);
+        }
+        res.send(event)
+    });
+})
+    .put(function (req, res) {
+        Event.findById(req.params.event_id,function (err, event) {
+            if(err){
+                res.send(err);
+            }
+            event.name = req.body.name;
+            event.timestamp = req.body.timestamp;
+            event.attending_count = req.body.attending_count;
+            event.interested_count = req.body.interested_count;
+            event.start_time = req.body.start_time;
+            event.end_time = req.body.end_time;
+            event.place = req.body.place;
+            event.is_private = req.body.is_private;
+            
+            event.save(function (err) {
+                if(err){
+                    res.send(err);
+                }
+                res.json({message:'Event Updated'})
+            })
+        })
+    })
+
+.delete(function (req, res) {
+    Event.remove({_id:req.params.event_id}, function (err, event) {
+        if(err){
+            res.send(err)
+        }
+
+        res.json('Successfully Deleted')
+    })
+});
+
+
+app.use('/api', router);
 app.listen(port);
-console.log("Listening on port "+port);
+console.log("Listening on port " + port);
