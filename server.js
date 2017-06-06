d6b534d440c6e450e8a05d7ff0430f7a0c2d9d8c
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-06-06 08:40:34 +0530

    server

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
 /**
  * Created by rishabhshukla on 06/06/17.
  */
+
+const express = require("express");
+const app= express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var router = express.Router();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+var port = 8100;
+
+router.get('/', function (req, res) {
+
+    res.json({message:'welcome to events-api!'})
+
+});
+
+app.use('/api',router);
+app.listen(port);
+console.log("Listening on port "+port);
